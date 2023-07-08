96439dfe3e7cb0a5b7927211e65695bccded02c2
Nathan Trautenberg <Natester123@mac.com>
2023-07-07 18:21:25 -0700

    add puts

--- variables.rb
+++ variables.rb
@@ -3,6 +3,10 @@ email = "helloturing@gmail.com"
 dob = "01011847"
 voucher = "12x34gh89"
 
+email = "turinghello@gmail.com"
+dob ="12121212"
+voucher = "12egg58white34"
+
 # integer variables
 age = 82
 number_of_friends = 240
@@ -15,5 +19,12 @@ pushups_per_second = 0.83
 
 # Boolean variables
 awake = true
-surfed today = true
-washed car = false
+surfed_today = true
+washed_car = false
+
+# puts statements
+puts "Hello," + email
+print "Is it true you surfed today?" 
+puts surfed_today
+print "How much pie did you eat?" 
+puts pie_consumed
